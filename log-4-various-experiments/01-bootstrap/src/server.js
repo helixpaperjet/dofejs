66addcfe2ed2f4c4eeb3172520113e3c928c314d
imsonmia <imsonmia@outlook.com>
2022-01-23 14:23:04 +0200

    modified server.js

--- log-4-various-experiments/01-bootstrap/src/server.js
+++ log-4-various-experiments/01-bootstrap/src/server.js
@@ -3,8 +3,10 @@ const app = express()
 const port = 3000
 app.use(express.static("index.html"))
 app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/index.html`)
+    res.type("text/javascript")
+    res.sendFile(`${__dirname}/generateJsonData.js`)
 })
+
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`)
 })
